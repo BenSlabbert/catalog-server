25057624046f3cf33f898553315b4811e5a4c471
benjamin.slabbert <benjamin.slabbert@gmail.com>
2023-01-27 21:32:37 +0200

    adding item module

--- item/src/main/java/com/fluent/item/ItemApplication.java
+++ item/src/main/java/com/fluent/item/ItemApplication.java
@@ -2,10 +2,12 @@ package com.fluent.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @EnableWebFlux
 @SpringBootApplication
+@EnableTransactionManagement
 public class ItemApplication {
 
   public static void main(String[] args) {
